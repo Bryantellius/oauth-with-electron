8161217400ec8c47b294155b82d10604bd2a2fbc
Ben <brbryant2639@gmail.com>
2021-12-03 16:22:41 -0600

    FIX: Changed app.on('ready') listener to reference

--- app/electron/main.js
+++ app/electron/main.js
@@ -16,7 +16,7 @@ async function showWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", showWindow());
+app.on("ready", showWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
